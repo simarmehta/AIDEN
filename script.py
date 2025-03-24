d4f47e83f31310028f89aa42593f6c74a72fcb89
Simardeep Singh Mehta <70880900+simarmehta@users.noreply.github.com>
2025-03-24 19:33:25 -0400

    Delete script.py

--- script.py
+++
@@ -1,6 +0,0 @@
-from embeddings import generate_embedding
-import json
-
-query = "Is React fast?"
-vector = generate_embedding(query)  # Should return a 768-dim list
-print(json.dumps(vector))  # Copy-paste this into SQL query
